42e4d543257f6c9c056939f859d461fc7fc63085
nukyma <mara.jounard@gmail.com>
2023-08-02 20:50:05 +0200

    Add TRACK_ID url

--- API/url_constants.py
+++ API/url_constants.py
@@ -1,3 +1,5 @@
 # URLS
 USER_PRIVATE_INFO = 'https://api.spotify.com/v1/me'
 USER_RECENTLY_PLAYED_TRACKS = 'https://api.spotify.com/v1/me/player/recently-played?limit=50'
+
+TRACK_ID = 'https://api.spotify.com/v1/tracks/'
