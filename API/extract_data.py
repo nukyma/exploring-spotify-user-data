d9d9ca8984a4d6667406e2ee35071ec4da26fa03
nukyma <mara.jounard@gmail.com>
2023-08-02 20:51:20 +0200

    Fill the methods info

--- API/extract_data.py
+++ API/extract_data.py
@@ -6,8 +6,8 @@ import spotify_urls
 def get_user_private_info(sp):
     """
     get user private profile information
-    :param sp:
-    :return: result
+    :param sp: authorize spotify session
+    :return type(json): private user info
     """
     endpoint_url = spotify_urls.USER_PRIVATE_INFO
     result = sp.get(endpoint_url)
@@ -18,8 +18,8 @@ def get_user_private_info(sp):
 def get_user_recently_played_tracks(sp):
     """
     get user last 50 played tracks
-    :param sp:
-    :return:
+    :param sp: authorize spotify session
+    :return type(json): items dict size 50 with tracks info
     """
     endpoint_url = spotify_urls.USER_RECENTLY_PLAYED_TRACKS
     result = sp.get(endpoint_url)
