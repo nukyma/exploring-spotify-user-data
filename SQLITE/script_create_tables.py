cef745dd8b9d59eb9e06cd0df5ed3326792a3dc4
nukyma <mara.jounard@gmail.com>
2023-08-02 14:25:54 +0200

    Delete duplicated code

--- SQLITE/script_create_tables.py
+++ SQLITE/script_create_tables.py
@@ -26,21 +26,6 @@ cur.execute("CREATE TABLE track("
             "is_saved INT "
             ")")
 
-cur.execute("CREATE TABLE track("
-            "id TEXT PRIMARY KEY, "
-            "name TEXT, "
-            "album_id TEXT, "
-            "artist TEXT, "
-            "duration_ms REAL, "
-            "explicit INT, "
-            "popularity INT, "
-            "type TEXT, "
-            "track_uri TEXT, "
-            "first_played TEXT, "
-            "last_played TEXT, "
-            "times_played INT "
-            ")")
-
 cur.execute("CREATE TABLE track_features("
             " track_id TEXT, "
             "acousticness REAL, "
