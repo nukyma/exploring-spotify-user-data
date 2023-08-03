1e8e859da78405dc2c8981832caee638b8bc96ed
nukyma <mara.jounard@gmail.com>
2023-08-03 13:36:41 +0200

    The API limit is 50, so we need to create batches of 50 tracks_ids to call the API the least number of times possible.

--- TRANSFORM/transform_data.py
+++ TRANSFORM/transform_data.py
@@ -11,3 +11,29 @@ def recently_played_tracks(data):
                              )
 
     return played_tracks
+
+
+def make_batches_of_tracks_ids(size, data):
+    list_batches = list()
+    counter = 0
+    string_ids = ""
+    for i in data:
+
+        if counter <= size - 1:
+            string_ids = string_ids + str(i) + ','
+            counter += 1
+        else:
+            # batch_ids expected format: "7ouMYWpwJ422jRcDASZB7P,4VqPOruhp5EdPBeR92t6lQ,2takcwOaAZWiXQijPHIx7B"
+            # Clean the ' at start and end of every id
+            string_ids = string_ids.replace("'", "")
+            # Include the first ' char
+            string_ids = "'" + string_ids
+            # Get rid of the last comma and add the last ' char
+            string_ids = string_ids[:-1] + "'"
+
+            list_batches.append(string_ids)
+
+            counter = 0
+            string_ids = ""
+
+    return list_batches
