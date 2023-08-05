b540c8deba1c43728c1cac91e82a53c0fefd81a2
nukyma <mara.jounard@gmail.com>
2023-08-05 12:04:17 +0200

    Divide into separate function the create tables commands

--- DATABASE/create_tables.py
+++ DATABASE/create_tables.py
@@ -2,49 +2,69 @@ import sqlite3
 
 import settings
 
-con = sqlite3.connect(settings.DB_ABSOLUTE_PATH)
-cur = con.cursor()
 
-cur.execute("CREATE TABLE play("
-            "track_id TEXT , "
-            "track_played_at TEXT , "
-            "context TEXT ,"
-            "PRIMARY KEY (track_id, track_played_at)"
-            ")")
+def create_play_table():
+    con = sqlite3.connect(settings.DB_ABSOLUTE_PATH)
+    cur = con.cursor()
 
-cur.execute("CREATE TABLE track("
-            "id TEXT PRIMARY KEY, "
-            "name TEXT, "
-            "album_id TEXT, "
-            "album_name TEXT, "
-            "artists_id TEXT, "
-            "artists_names TEXT, "
-            "duration_ms REAL, "
-            "explicit INT, "
-            "popularity INT, "
-            "type TEXT, "
-            "track_uri TEXT, "
-            "first_played TEXT, "
-            "last_played TEXT, "
-            "times_played INT, "
-            "is_saved INT "
-            ")")
+    cur.execute("CREATE TABLE play("
+                "track_id TEXT , "
+                "track_played_at TEXT , "
+                "context TEXT ,"
+                "PRIMARY KEY (track_id, track_played_at)"
+                ")")
+    con.commit()
+    con.close()
 
-cur.execute("CREATE TABLE track_features("
-            " track_id TEXT PRIMARY KEY, "
-            "acousticness REAL, "
-            "analysis_url TEXT, "
-            "danceability REAL, "
-            "duration_ms INT, "
-            "energy REAL, "
-            "instrumentalness REAL, "
-            "key INT, "
-            "liveness REAL, "
-            "loudness REAL, "
-            "mode INT, "
-            "valence REAL,  "
-            "speechiness REAL, "
-            "tempo REAL, "
-            "time_signature INT, "
-            "track_href TEXT "
-            ")")
+
+def create_track_table():
+    con = sqlite3.connect(settings.DB_ABSOLUTE_PATH)
+    cur = con.cursor()
+
+    cur.execute("CREATE TABLE track("
+                "id TEXT PRIMARY KEY, "
+                "name TEXT, "
+                "album_id TEXT, "
+                "album_name TEXT, "
+                "artists_id TEXT, "
+                "artists_names TEXT, "
+                "duration_ms REAL, "
+                "explicit INT, "
+                "popularity INT, "
+                "type TEXT, "
+                "track_uri TEXT, "
+                "first_played TEXT, "
+                "last_played TEXT, "
+                "times_played INT, "
+                "is_saved INT "
+                ")")
+
+    con.commit()
+    con.close()
+
+
+def create_track_features_table():
+    con = sqlite3.connect(settings.DB_ABSOLUTE_PATH)
+    cur = con.cursor()
+
+    cur.execute("CREATE TABLE track_features("
+                " track_id TEXT PRIMARY KEY, "
+                "acousticness REAL, "
+                "analysis_url TEXT, "
+                "danceability REAL, "
+                "duration_ms INT, "
+                "energy REAL, "
+                "instrumentalness REAL, "
+                "key INT, "
+                "liveness REAL, "
+                "loudness REAL, "
+                "mode INT, "
+                "valence REAL,  "
+                "speechiness REAL, "
+                "tempo REAL, "
+                "time_signature INT, "
+                "track_href TEXT "
+                ")")
+
+    con.commit()
+    con.close()
