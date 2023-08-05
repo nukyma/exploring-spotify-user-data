cf391d3e934bc78c0304ab782d436497e06ce597
nukyma <mara.jounard@gmail.com>
2023-08-05 22:48:21 +0200

    Implement the use of try except and logs in the main function to avoid undesired behaviours. Call the function that now saves the response data in 4 different variables for it posterior load in DB

--- main.py
+++ main.py
@@ -2,34 +2,37 @@ from API import extract_data, connection
 from DATABASE import play_table, track_table, audio_features_table
 from TRANSFORM import transform_data
 
+import logging
+
+import settings
+
 if __name__ == '__main__':
 
-    spotify, token = connection.connect_spotify_api()
+    logging.basicConfig(level=logging.INFO, filename=settings.LOGS_FILE_LOCATION, format='%(asctime)s  -  %(message)s')
+
+    # Stablish connection (authentication and authorization) with Spotify API
+    try:
+        spotify, token = connection.connect_spotify_api()
+    except ConnectionError:
+        logging.info('Error connecting with Spotify API')
 
     # Use own methods to pull raw data from the Spotify API
     # user_info = extract_data.get_user_private_info(sp=spotify)
 
     ###         USER LAST 50 PLAYED SONGS                       ###
     # Extract
-    played_tracks = extract_data.get_user_recently_played_tracks(sp=spotify)
-    # Transform: Select, clean and transform data
-    played_tracks_dict = transform_data.recently_played_tracks(data=played_tracks)
-    # Load
-    play_table.insert_into_play(data=played_tracks_dict)
+    try:
+        played_tracks = extract_data.get_user_recently_played_tracks(sp=spotify)
+    except RuntimeError:
+        logging.info('ENDPOINT NOT WORKING: get_user_recently_played_tracks ')
 
-    ###         COMPLETE TRACKS INFO IN BATCHES OF 50           ###
-    # Consult our DB
-    list_tracks_ids = track_table.get_tracks_incomplete()
-    # BATCH
-    list_batch_ids = transform_data.make_batches_of_tracks_ids(size=50, data=list_tracks_ids)
-    # Extract
-    info_tracks = list()
-    for i in list_batch_ids:
-        info_tracks.append(extract_data.get_several_tracks_info(sp=spotify, batch_ids=i))
-    # Transform
-    info_tracks_dict = transform_data.tracks_info(data=info_tracks)
-    # Load
-    track_table.insert_into_track(data=info_tracks_dict)
+    # If the response is OK
+    try:
+        # Select, clean and transform data from the response to populate different tables
+        played_tracks, track_info, map_track_album_info, map_track_artist_info = (
+            transform_data.recently_played_tracks_response(data=played_tracks))
+    except ValueError:
+        logging.info('ENDPOINT RESPONSE: get_user_recently_played_tracks status: ', played_tracks['error']['status'])
 
     ###         COMPLETE AUDIO FEATURES INFO IN BATCHES OF 50    ###
     # Consult our DB
