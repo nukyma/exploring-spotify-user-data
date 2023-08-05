3165cddfe182bc0eb639b6a6160c9f14e30cd76a
nukyma <mara.jounard@gmail.com>
2023-08-05 14:43:36 +0200

    Include SEVERAL_AUDIO_FEATURES endpoint

--- API/endpoints.py
+++ API/endpoints.py
@@ -7,3 +7,6 @@ USER_RECENTLY_PLAYED_TRACKS = 'https://api.spotify.com/v1/me/player/recently-pla
 
 # https://developer.spotify.com/documentation/web-api/reference/get-several-tracks
 SEVERAL_TRACKS_ID = 'https://api.spotify.com/v1/tracks/?ids='
+
+# https://developer.spotify.com/documentation/web-api/reference/get-several-audio-features
+SEVERAL_AUDIO_FEATURES = 'https://api.spotify.com/v1/audio-features?ids='
