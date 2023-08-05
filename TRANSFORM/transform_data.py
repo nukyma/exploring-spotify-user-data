1a22f315e652fc58596d2223967530487c47dfcd
nukyma <mara.jounard@gmail.com>
2023-08-05 22:46:03 +0200

    Get data from endpoint response and save it in different variable for its posterior load in the data base

--- TRANSFORM/transform_data.py
+++ TRANSFORM/transform_data.py
@@ -1,16 +1,58 @@
-def recently_played_tracks(data):
-    played_tracks = list()
-    for i in data['items']:
-        track_id = i['track']['id']
-        played_at = i['played_at'].replace('T', ' ')
-        context = i['context']['type']
+def recently_played_tracks_response(data):
+    """
+    get as much data as possible from the response to get_user_recently_played_tracks
+    :param data: response
+    :return:
+    """
 
-        played_tracks.append({'track_id': track_id,
-                              'played_at': played_at,
-                              'context': context}
+    # played_tracks for PLAY table
+    played_tracks = list()
+    map_track_album_info = list()
+    map_track_artist_info = list()
+    for i in data['items']:
+        played_tracks.append({'track_id': i['track']['id'],
+                              'played_at': i['played_at'].replace('T', ' '),
+                              'context': i['context']['type']}
                              )
 
-    return played_tracks
+        map_track_album_info.append({
+            'track_id': i['track']['id'],
+            'album_id': i['track']['album']['id'],
+            'played_at': i['played_at'].replace('T', ' '),
+            'context': i['context']['type'],
+        })
+
+        for k in i['tracks']['artists']:
+            map_track_artist_info.append({'track_id': i['track']['id'],
+                                          'artist_id': k['id'],
+                                          'played_at': i['played_at'].replace('T', ' '),
+                                          'context': i['context']['type']
+                                          })
+
+    # track_info for TRACK table
+    track_info = list()
+    for j in data['items']:
+
+        artists_id = list()
+        artists_names = list()
+        for m in j['track']['artists']:
+            artists_id.append(m['id'])
+            artists_names.append(m['name'])
+
+        track_info.append({'id': j['track']['id'],
+                           'name': j['track']['name'],
+                           'album_id': j['track']['album']['id'],
+                           'album_name': j['track']['album']['name'],
+                           'artists_id': str(artists_id),
+                           'artists_names': str(artists_names),
+                           'duration_ms': j['track']['duration_ms'],
+                           'explicit': j['track']['explicit'],
+                           'popularity': j['track']['popularity'],
+                           'type': j['track']['type'],
+                           'preview_url': j['track']['external_urls']['spotify']
+                           })
+
+    return played_tracks, track_info, map_track_album_info, map_track_artist_info
 
 
 def make_batches_of_tracks_ids(size, data):
@@ -64,7 +106,6 @@ def tracks_info(data):
                      'artists_names': str(artists_names),
                      'duration_ms': i['duration_ms'],
                      'explicit': i['explicit'],
-                     'popularity': i['popularity'],
                      'type': i['type'],
                      'preview_url': i['preview_url']
                      }
